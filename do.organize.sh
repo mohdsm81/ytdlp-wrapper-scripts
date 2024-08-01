438d36bfbfc47f8cbe90e749dde39596347460c9
Mohammed S. Al-Mahfoudh <mohammed.mahfoudh@gmail.com>
2024-08-01 15:28:50 -0600

    saving an organizer script to use in downloading full channels

---
+++ do.organize.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+
+FILES=()
+for file in *.{description,mp4,webp};
+do
+    f=$(basename -a $file)
+    FILES+=("$f");
+done
+
+# extract unique folder names (list names)
+FOLDERS=()
+for file in "${FILES[@]}";
+do
+    fldr=$(echo $file | awk -F" - " '{print $2;}')
+    FOLDERS+=("$fldr")
+done
+
+UNIQUE_FOLDERS=($(echo "${FOLDERS[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+# echo ${FOLDERS[2]};
+# echo ${UNIQUE_FOLDERS[@]};
+
+#create all folders, if they don't exist
+for folder in "${UNIQUE_FOLDERS[@]}";
+do
+    if [ ! -d $folder ] && [[ $folder != "" ]];
+    then
+        echo "creating folder: $folder"
+        mkdir $folder
+    fi
+done
+
+# Move files to their respective folders
+for folder in "${FOLDERS[@]}";
+do
+    if [ -d $folder ] && [[ $folder != "" ]]; then
+        # echo *$folder*.*
+        # This one is erroneous (tries to move the folder inside of itself)
+        #$(mv *$folder*.* $folder)
+        $(find . -mindepth 1 -maxdepth 1 -name "*$folder*.*" -a -not -name "$folder" -exec mv -t $folder {} +)
+    fi
+done
