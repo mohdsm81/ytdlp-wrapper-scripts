1de6d308cee98a21e30046228d95692c81dd27c2
Mohammed S. Al-Mahfoudh <mohammed.mahfoudh@gmail.com>
2024-08-01 15:33:31 -0600

    added another automation script

---
+++ yank.channel.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# download all playlists in the channel
+./download-list "$@"
+
+# re-organize files (creating a folder per playlist and moving files into their
+# respective folder/playlist)
+./do.organize.sh
