6fbfe6b9efa69688b8e42edddca290da9c3e426b
Mohammed S. Al-Mahfoudh <mohammed.mahfoudh@gmail.com>
2024-08-01 16:20:53 -0600

    fixed on my desktop, lets see if it works on the server

--- do.organize.sh
+++ do.organize.sh
@@ -3,33 +3,42 @@
 FILES=()
 for file in *.{description,mp4,webp};
 do
-    f=$(basename -a "$file")
-    FILES+=("$f")
+    f=$(basename -a $file)
+    FILES+=("$f");
 done
 
-# Extract unique folder names (list names)
+# extract unique folder names (list names)
 FOLDERS=()
 for file in "${FILES[@]}";
 do
-    fldr=$(echo "$file" | awk -F" - " '{print $2;}')
+    fldr=$(echo $file | awk -F" - " '{print $2;}')
     FOLDERS+=("$fldr")
 done
 
-UNIQUE_FOLDERS=($(echo "${FOLDERS[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+# UNIQUE_FOLDERS=($(echo "${FOLDERS[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
 
-# Create all folders, if they don't exist
+# Get unique folder names and store them in an array
+mapfile -t UNIQUE_FOLDERS < <(printf "%s\n" "${FOLDERS[@]}" | sort -u)
+# echo ${FOLDERS[2]};
+# echo ${UNIQUE_FOLDERS[@]};
+
+#create all folders, if they don't exist
 for folder in "${UNIQUE_FOLDERS[@]}";
 do
-    if [ ! -d $folder ] && [[ $folder != "" ]]; then
-        echo "creating folder: $folder"
+    if [ ! -d "$folder" ] && [[ "$folder" != "" ]];
+    then
+        echo "creating folder: '$folder'"
         mkdir "$folder"
     fi
 done
 
 # Move files to their respective folders
-for file in "${FILES[@]}"; do
-    folder=$(echo "$file" | awk -F" - " '{print $2;}')
-    if [ -d $folder ] && [[ $folder != "" ]]; then
-        find . -mindepth 1 -maxdepth 1 -name "*$file*" -a -not -name "$folder" -exec mv -t "$folder" {} +
+for folder in "${UNIQUE_FOLDERS[@]}";
+do
+    if [[ -d "$folder" ]] && [[ "$folder" != "" ]]; then
+        # echo *$folder*.*
+        # This one is erroneous (tries to move the folder inside of itself)
+        # $ (mv *$folder*.* $folder)
+        find . -mindepth 1 -maxdepth 1 -name "*$folder*.*" -a -not -name "$folder" -exec mv -t "$folder" {} +
     fi
 done
