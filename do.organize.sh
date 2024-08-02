c7f53f0bbe32a19779b1a9aab8b2b65fff625495
Mohammed S. Al-Mahfoudh <mohammed.mahfoudh@gmail.com>
2024-08-01 21:23:32 -0600

    improved do.organize.sh

--- do.organize.sh
+++ do.organize.sh
@@ -15,30 +15,33 @@ do
     FOLDERS+=("$fldr")
 done
 
-# UNIQUE_FOLDERS=($(echo "${FOLDERS[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
-
 # Get unique folder names and store them in an array
 mapfile -t UNIQUE_FOLDERS < <(printf "%s\n" "${FOLDERS[@]}" | sort -u)
-# echo ${FOLDERS[2]};
-# echo ${UNIQUE_FOLDERS[@]};
 
 #create all folders, if they don't exist
+echo "Creating folders: "
 for folder in "${UNIQUE_FOLDERS[@]}";
 do
     if [ ! -d "$folder" ] && [[ "$folder" != "" ]];
     then
-        echo "creating folder: '$folder'"
+        echo "'$folder'"
         mkdir "$folder"
     fi
 done
 
+
+
 # Move files to their respective folders
+echo "Moving files to their respective folder (i.e. a folder per playlist)"
 for folder in "${UNIQUE_FOLDERS[@]}";
 do
     if [[ -d "$folder" ]] && [[ "$folder" != "" ]]; then
-        # echo *$folder*.*
-        # This one is erroneous (tries to move the folder inside of itself)
-        # $ (mv *$folder*.* $folder)
         find . -mindepth 1 -maxdepth 1 -name "*- $folder -*.*" -a -not -name "$folder" -exec mv -t "$folder" {} +
     fi
 done
+
+# Move all folders containing the files into the channel folder
+# Move files to their respective folders
+CHANNEL_FOLDER=$(echo $FILES[0] | awk -F ' - ' '{print $1;}')
+echo "Moving all folders to the channel folder: '$CHANNEL_FOLDER'"
+find . -mindepth 1 -maxdepth 1 -not -name "*.sh" -a -not -name "$CHANNEL_FOLDER" -exec mv -t "$CHANNEL_FOLDER" {} +
