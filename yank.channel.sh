3cdf1f2d7f89dcfb118c6b6a58b3d6aaf9017f45
Mohammed S. Al-Mahfoudh <mohammed.mahfoudh@gmail.com>
2024-08-02 17:23:43 -0600

    let me try this

--- yank.channel.sh
+++ yank.channel.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # download all playlists in the channel
-./download-list "$@"
+/usr/local/bin/download-list "$@"
 
 # re-organize files (creating a folder per playlist and moving files into their
 # respective folder/playlist)
-./do.organize.sh
+/usr/local/bin/do.organize.sh
