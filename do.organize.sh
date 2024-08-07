58b102ea6e8ff3fbe9a48e6e5ae92b3bb37b6957
Mohammed S. Al-Mahfoudh <mohammed.mahfoudh@gmail.com>
2024-08-07 05:45:40 -0600

    I think this fixes it

--- do.organize.sh
+++ do.organize.sh
@@ -60,9 +60,9 @@ do
             # delete local folder
             rmdir "$folder"
         else
-            echo "Moving folder $folder to $CHANNEL_FOLDER"
+            echo "Moving folder $folder == to ==> $CHANNEL_FOLDER"
             # move the local folder itself inside the channel folder
-            mv "$folder" "$CHANNEL_FOLDER"
+            mv "$folder" "$CHANNEL_FOLDER/"
         fi
     fi
 done
